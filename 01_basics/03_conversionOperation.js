36e156756e755992fa9a89d7f1d3f5784480aa66
Prashant Kumar <139355272+Prashant1062003@users.noreply.github.com>
2023-08-27 07:18:45 +0000

    conversion of dataTypes.

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33";
+
+
+// console.log(typeof score); 
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+// "33" => 33
+// "33des" => NaN
+// true => 1; false => 0;
+
+// let isLoggedIn = ""; -----> false
+let isLoggedIn = 1; //-----> true
+// let isLoggedIn = "Prashant"; //-----> false
+// let isLoggedIn = 0; //-----> false
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// " "; => false
+// "Prashant; => false
+
+let secNum = 12;
+
+let stringNum = String(secNum);
+console.log(stringNum);
+console.log(typeof stringNum);
