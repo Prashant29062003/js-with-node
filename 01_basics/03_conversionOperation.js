79a9dbf74c71464321c57da9561957645c7af22c
Prashant Kumar <139355272+Prashant1062003@users.noreply.github.com>
2023-08-27 07:39:09 +0000

    conversion operation some more features

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,5 +28,33 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let secNum = 12;
 
 let stringNum = String(secNum);
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+// ******************** Operations *******************
+
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "Prashant"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2) // => 12
+// console.log(1 + "2")    // => 12
+// console.log("1" + 2 + 2) // => 122
+// console.log(1 + 2 + "2") // => 32
+
+// console.log(true) => true
+// console.log(+true) // => 1
+// console.log(+"") // => false
+
