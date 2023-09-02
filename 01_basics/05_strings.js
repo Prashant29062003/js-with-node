a78007ae4d57393610f8eafd75af3ecda7fac3f4
Prashant29062003 <112088696+Prashant29062003@users.noreply.github.com>
2023-09-02 06:19:35 +0000

    string chapter staarted

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "Prashant Kumar"
+const repoCount = 8
+
+console.log(name + repoCount + " Value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //-----> Sting Interpolation => `${}`
+
+console.log(typeof name) // ----> String => Object
+
+const gameName = new String ("Codingko")
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf("i"));
+
+const newString = gameName.substring
