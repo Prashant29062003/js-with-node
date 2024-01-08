71b4a87397dc11042b73069e5f9c3cba02700a43
Prashant29062003 <112088696+Prashant29062003@users.noreply.github.com>
2024-01-08 08:12:59 +0000

    function basics

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,17 @@
+
+
+function sayMyName(){
+    console.log("P")
+    console.log("r")
+    console.log("a")
+    console.log("s")
+    console.log("h")
+    console.log("a")
+    console.log("n")
+    console.log("t")
+}
+
+// sayMyName(); // function call
+// sayMyName; //referencing
+
+
