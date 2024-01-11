8172b7515a733d46fd4c073175f7a49b859d60d2
Prashant29062003 <112088696+Prashant29062003@users.noreply.github.com>
2024-01-11 12:22:35 +0000

    arrow function's explicit and implicit return value defination and understading

--- 03_basics/03_arrowFuncion.js
+++ 03_basics/03_arrowFuncion.js
@@ -38,13 +38,20 @@ nimbu();
 // } 
 
 
-// const addTwo = (num1, num2) => num1 + num2
+// const addTwo = (num1, num2) => num1 + num2   // this will return implicit value if arrow function has has expression body
 
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+// const addTwo = (num1, num2) => ( num1 + num2 )   // this will return implicit value if arrow function has has expression body
+
+const addTwo = (num1, num2) => {
+    num1 + num2
+}    // this has to return explicit value if arrow function has has block body
 
 
-const addTwo = (num1, num2) => ( {username: "Prahant"} )
+// const addTwo = (num1, num2) => ( {username: "Prahant"} )    // this will return implicit value if arrow function has has expression body
 
+// *********************************************
+// Arrow functions can have either an expression body or the usual block body. In an expression body, only a single expression is specified, which becomes the implicit return value. In a block body, you must use an explicit return statement.
+// ***********************************************
 
 console.log(addTwo(3,4));
